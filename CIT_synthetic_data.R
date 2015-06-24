2cd5b4d1b7ef56e80e86634d445c73c70d058f42
ariellacohain <ariella@cohain.us>
2015-06-24 14:48:58 -0400

    updated CIT to work and added permutation testing

--- CIT_synthetic_data.R
+++ CIT_synthetic_data.R
@@ -1,13 +1,21 @@
 rm(list=ls())
-### TESTING
 options(stringsAsFactors = F)
 
-threads = 1
+args <- commandArgs(trailingOnly=TRUE)
+
+workingDir = "/sc/orga/scratch/cohaia01/NetworkMethodsEvaluation" #"~/Dropbox/Network Methods Evaluation/"
+
+dataName = args[[1]]
+
+threads = 5
 threads_cit = 10
-dataName = c() #Param$NETID[1]
+perm_threads = 10
+numPermTests = 1000
+
 cis_fdr_cutoff = 0.20
 trans_fdr_cutoff = 0.20
 cor_pval_cutoff = 0.05
+
 
 library(citpp)
 library(foreach)
@@ -17,11 +25,11 @@ options(cores = multicore:::detectCores())
 registerDoMC(threads) 
 
 #setting work directory
-setwd("~/Dropbox/Network Methods Evaluation/")
+setwd(workingDir)
 #loading Param 
 load("SimulatedDataParameters.RData")
-str(Param)
-dataName = Param$NETID[1] ### EDIT THIS OUT // JUST FOR TESTING
+# str(Param)
+# dataName = Param$NETID[1] ### EDIT THIS OUT // JUST FOR TESTING
 
 cat("data --",dataName,"\n")
 cat("run ",which(Param$NETID == dataName ), "out of",length(Param$NETID),"\n")
@@ -108,17 +116,24 @@ getConditionalResults<-function(cis_eqtl_data,snpNameInterest,geneME = geneME,sn
 
 cond_results_all = c()
 correlations = c()
+all_trios = c()
 for(snp_id in unique(sig_cis$snps)){
   #checking for every snp that has a cis eQTL, how many genes are called cis, and how many trans
   cis_genes = sig_cis$gene[sig_cis$snps == snp_id]
   trans_genes = sig_trans$gene[sig_trans$snps == snp_id]
-  cat("---",snp_id,"number cis:",length(cis_genes),"number trans:",length(trans_genes),"---\n" )
+  if(which( unique(sig_cis$snps) == snp_id)%%10 ==0){
+    cat("--RUN",which(snp_id == unique(sig_cis$snps)),"out of",length(unique(sig_cis$snps)),"--\n")
+    cat("---",snp_id,"number cis:",length(cis_genes),"number trans:",length(trans_genes),"---\n" )
+      
+  }
+
+  
   #if SNP has at least 1 cis and at least 1 trans genes continue
   if(length(cis_genes)>0 & length(trans_genes)>0){
     #running conditional analysis for every cis gene that is present:
       cond_res = foreach(i  = 1:length(cis_genes),combine=rbind)%dopar%{
         cis.gene = cis_genes[i]
-        cat("on run ",i," / ",length(cis_genes),"\n")
+        # cat("on run ",i," / ",length(cis_genes),"\n")
         cisQTL_data = sig_cis[sig_cis$gene == cis.gene,]
         getConditionalResults(cisQTL_data,snp_id,expr,snp)  
       }
@@ -143,7 +158,9 @@ for(snp_id in unique(sig_cis$snps)){
       colnames(cis_data) = cis_genes
     }
     trans_data = as.matrix(expr[,is.element(colnames(expr),trans_genes)])
-    
+    if(length(trans_genes)==1){
+      colnames(trans_data) = trans_genes
+    }
     for(i in 1:ncol(cis_data)){
       g.vec = unlist(cis_data[,i])
       cor = apply(trans_data, 2, FUN=function(e,g)
@@ -156,8 +173,12 @@ for(snp_id in unique(sig_cis$snps)){
       correlations=rbind(correlations,cbind(colnames(cis_data)[i],colnames(trans_data),cor.rsqr,cor.pv))
     } # end of correlation for loop
     
+    trio = list(cis = cis_genes, trans = trans_genes)
+    all_trios[[snp_id]] = trio
+    cond_results_all = rbind(cond_results_all,cond_results)
+    
     } # end of cis and trans at least 1 hit if statement 
-  cond_results_all = rbind(cond_results_all,cond_results)
+  
 
 } # end of loop over all snps with cis QTL
 
@@ -169,18 +190,41 @@ correlations$cor.pv = as.numeric(correlations$cor.pv)
 
 
 ## setting CITPP indexes and which tests to run:
+snp_position = seq(1:ncol(snp))
+names(snp_position) = colnames(snp)
 
-L = 
-Cis = 
-Trans = 
+expr_position = seq(1:ncol(expr))
+names(expr_position) = colnames(expr)
 
-trios = 
+L = snp
+Cis = expr
+Trans = expr
 
-output = cit(L,Cis,Trans, trios, threads = threads_cit)
+trios_tmp = sapply(names(all_trios),function(x){
+  snp_col = snp_position[x]
+  # cat(names(x))
+  cis_position = expr_position[all_trios[[x]]$cis]
+  trans_position = expr_position[all_trios[[x]]$trans]
+  cbind(snp_col,cis_position,trans_position)
+  })
+
+trios = data.frame(do.call(rbind,trios_tmp))
+
+trios$cor_pval = unlist(lapply(seq(1:nrow(trios)),function(x){
+  correlations$cor.pv[correlations$Cis_gene == colnames(expr)[trios$cis_position[x]] & correlations$Trans_gene==colnames(expr)[trios$trans_position[x]]]
+  }))
+trios$cor_rsqr = unlist(lapply(seq(1:nrow(trios)),function(x){
+  correlations$cor.rsqr[correlations$Cis_gene == colnames(expr)[trios$cis_position[x]] & correlations$Trans_gene==colnames(expr)[trios$trans_position[x]]]
+  }))
+
+
+trios = trios[trios$cor_pval <= cor_pval_cutoff,]
+
+output = cit(L,Cis,Trans, trios[,c(1,2,3)], threads = threads_cit)
 
 cat("cit finished.\n")
 
-cit_LTC = cit(L,Trans,Cis,trios[,c(1,3,2)],threads=threads)
+cit_LTC = cit(L,Trans,Cis,trios[,c(1,3,2)],threads=threads_cit)
 colnames(cit_LTC)= paste(colnames(cit_LTC),"reactive",sep="_")
 cat("running cit L->T->C finished.\n")
 output= cbind(output,cit_LTC)
@@ -191,28 +235,127 @@ output$snp_id = colnames(L)[output$L_index] #rep(snp.of.interest[1],nrow(cit))
 output$cis_gene = colnames(Cis)[output$G_index]
 output$trans_gene = colnames(Trans)[output$T_index]
 
-output$max_snp_chr_pos = cond_results$most_sig_snp[output$G_index]
-output$max_snp_pval = cond_results$sig_snp_pval[output$G_index]
-output$cis_pvalue = cond_results$gwas_cis_pvalue[output$G_index]
-output$cond_pval = cond_results$cond_pval[output$G_index]
-output$cond_rsqd = cond_results$cond_rsqd[output$G_index]
+output$max_snp = sapply(1:nrow(output),function(x){
+  cond_results_all$most_sig_snp[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
 
-output$cond_rsqd_MaxGvnGwas = cond_results$cond_rsqd_MaxGvnGwas[output$G_index]
-output$cond_pval_MaxGvnGwas = cond_results$cond_pval_MaxGvnGwas[output$G_index]
+output$max_snp_pval = sapply(1:nrow(output),function(x){
+  cond_results_all$sig_snp_pval[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
 
-output$rsqr_TassocG = unlist(lapply(seq(1:nrow(output)),function(x){correlations$cor.rsqr[correlations$Cis_gene == output$cis_gene[x] & correlations$Trans_gene==output$trans_gene[x]]}))
-output$p_TassocG_pre = unlist(lapply(seq(1:nrow(output)),function(x){correlations$cor.pv[correlations$Cis_gene == output$cis_gene[x] & correlations$Trans_gene==output$trans_gene[x]]}))
+output$cis_pvalue = sapply(1:nrow(output),function(x){
+  cond_results_all$gwas_cis_pvalue[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
 
-output$cisQTL_fdr = 
-output$transQTL_fdr = 
 
-cat("running REACTIVE cit (L->T->C)...\n")
-cit_LTC = cit(L,Trans,Cis,trios[,c(1,3,2)],threads=threads)
-colnames(cit_LTC)= paste(colnames(cit_LTC),"reactive",sep="_")
-cat("running cit L->T->C finished.\n")
-output= cbind(output,cit_LTC)
+output$cond_pval = sapply(1:nrow(output),function(x){
+  cond_results_all$cond_pval[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
+
+
+output$cond_rsqd = sapply(1:nrow(output),function(x){
+  cond_results_all$cond_rsqd[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
+
+output$cond_rsqd_MaxGvnGwas = sapply(1:nrow(output),function(x){
+  cond_results_all$cond_rsqd_MaxGvnGwas[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
+
+output$cond_pval_MaxGvnGwas = sapply(1:nrow(output),function(x){
+  cond_results_all$cond_pval_MaxGvnGwas[
+    cond_results_all$snp == output$snp_id[x] &
+    cond_results_all$cis_gene == output$cis_gene[x]
+  ] })
+
+output$rsqr_TassocG = trios$cor_rsqr
+output$p_TassocG_pre = trios$cor_pval
+
+output$cisQTL_fdr =sapply(1:nrow(output),function(x){
+  sig_cis$FDR[
+    sig_cis$snps == output$snp_id[x] &
+    sig_cis$gene == output$cis_gene[x]
+  ] })
+output$transQTL_fdr = sapply(1:nrow(output),function(x){
+  sig_trans$FDR[
+    sig_trans$snps == output$snp_id[x] &
+    sig_trans$gene == output$trans_gene[x]
+  ] })
+
+# cat("running REACTIVE cit (L->T->C)...\n")
+# cit_LTC = cit(L,Trans,Cis,trios[,c(1,3,2)],threads=threads)
+# colnames(cit_LTC)= paste(colnames(cit_LTC),"reactive",sep="_")
+# cat("running cit L->T->C finished.\n")
+# output= cbind(output,cit_LTC)
+
+### NEXT STEP IS TO THEN RUN PERMUTATIONS 
+permute <- function(snp,gene1,gene2,ExprData, time = Sys.time(), maxit=50000,  n=10000,threads){
+  set.seed(as.numeric(time))
+
+  # rand_snps=sapply(1:n,function(X){snp[sample(seq(1:length(snp)))]})
+  pos_gene2 = which(colnames(ExprData)==gene2)
+  trans = ExprData[,pos_gene2]
+  L = sapply(snp, floor) #round or ceiling
+
+  shuffTrans = sapply(1:n, function(x){
+    res = rep(Inf, length(trans))
+    for(j in unique(L)){
+      res[L==j] = sample(trans[L==j])
+    }
+    return(res)
+    })
+
+  # trans_gene = sapply(1:n,function(X){ExprData[sample(seq(1:nrow(geneExpr))),pos_gene2]})
+
+  l_index = 1
+  c_index = rep(which(colnames(ExprData)== gene1),n) 
+  t_index = 1:n
+
+  trios = cbind(l_index,c_index,t_index)
+  cit = cit(as.matrix(L),ExprData,shuffTrans,trios,maxit=maxit,threads=threads)
+
+  return(cit)
+
+}
+
+perm_pvals = c()
+for(i in 1:nrow(output)){
+  # if(i %% 10 ==0 ){
+
+    cat("on run",i, "out of",nrow(output),"\n")
+    print(Sys.time())
+
+  # }
+  snp_i = snp[,c(output$snp_id[i])]
+  gene1 = output$cis_gene[i]
+  gene2 = output$trans_gene[i]
+
+  #run permutation test on snp, gene1, gene2 - returns matrix from cit with N rows corresponding to the number permutations
+  permuted_res <- permute(snp_i,gene1,gene2,expr, maxit =50000, n = numPermTests,threads=perm_threads)
+
+  # calculating how many permuted tests are more significant than observed (b+1)/(N+1) where b = # permuted < observed
+  permuted_pval = (sum(permuted_res$p_cit < output$p_cit[i])+1)/ (numPermTests  +1)
+  perm_pvals = c(perm_pvals,permuted_pval)
+}
+
+output$perm_pval = perm_pvals
+output$fdr = p.adjust(perm_pvals,"fdr")
+
+output$passSnpThresh = output$cond_pval <=0.05 | (output$cond_pval>0.05 & output$cond_pval_MaxGvnGwas >0.05) | output$snp_id == output$max_snp
 
 save(L,Cis,Trans,trios,output, file=paste("SimulatedData/",dataName,"/cit_results.RData",sep=""))
 
-#testing commit
-### NEXT STEP IS TO THEN RUN PERMUTATIONS 
+sig_output = output[output$p_cit_reactive >0.05 & output$passSnpThresh, ]
+write.table(sig_output,file = paste("SimulatedData/",dataName,"/cit_results_filtered.txt",sep=""))
+
